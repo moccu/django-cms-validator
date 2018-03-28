395bbcf317d83d2f421afd37ef0b42f9892a4d66
Norman Rusch <norman.rusch@moccu.com>
2018-03-28 18:59:21 +0200

    Use link rules in defaults

--- static/js/rules/defaults.js
+++ static/js/rules/defaults.js
@@ -1,1 +1,14 @@
-export const defaults = [];
+import {LinkNoDescriptiveTitleRule} from './LinkNoDescriptiveTitle';
+import {LinkNoLabelRule} from './LinkNoLabel';
+import {LinkNoRelNoopenerRule} from './LinkNoRelNoopener';
+import {LinkNoRelNoreferrerRule} from './LinkNoRelNoreferrer';
+import {LinkNoTitleRule} from './LinkNoTitle';
+
+
+export const defaults = [
+	LinkNoDescriptiveTitleRule,
+	LinkNoLabelRule,
+	LinkNoRelNoopenerRule,
+	LinkNoRelNoreferrerRule,
+	LinkNoTitleRule
+];
--- static/js/rules/defaults.test.js
+++ static/js/rules/defaults.test.js
@@ -1,11 +1,20 @@
 import {defaults} from './defaults';
+import {LinkNoDescriptiveTitleRule} from './LinkNoDescriptiveTitle';
+import {LinkNoLabelRule} from './LinkNoLabel';
+import {LinkNoRelNoopenerRule} from './LinkNoRelNoopener';
+import {LinkNoRelNoreferrerRule} from './LinkNoRelNoreferrer';
+import {LinkNoTitleRule} from './LinkNoTitle';
 
 
 describe('The rules defaults list', () => {
 
 	test('should contain all rules', () => {
 		expect(defaults).toEqual([
-
+			LinkNoDescriptiveTitleRule,
+			LinkNoLabelRule,
+			LinkNoRelNoopenerRule,
+			LinkNoRelNoreferrerRule,
+			LinkNoTitleRule
 		]);
 	});
 
